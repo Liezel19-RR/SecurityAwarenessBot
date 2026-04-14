aaf78433fc62148bf765596d91f0202800385f86
Liezel19-RR <liezelramela@icloud.com>
2026-04-14 14:26:39 +0200

    Add files via upload

---
+++ TpingEffect.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CyberSecurityBot
+{
+    internal class TypingEffect
+    {
+        public void Type(string message)
+        {
+            foreach (char c in message)
+            {
+                Console.Write(c);
+                Thread.Sleep(25);
+            }
+            Console.WriteLine();
+        }
+    }
+}
