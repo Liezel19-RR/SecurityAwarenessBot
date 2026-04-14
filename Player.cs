332448f42196a18ba9517488a17f724d9a7cb1f3
Liezel19-RR <liezelramela@icloud.com>
2026-04-14 14:21:36 +0200

    player
    player

---
+++ Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberSecurityBot
+{
+    internal class Player
+    {
+        public void Play(string path)
+        {
+            try
+            {
+                SoundPlayer player = new SoundPlayer(path);
+                player.Play(); 
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error playing sound: " + ex.Message);
+                Console.ResetColor();
+            }
+        }
+    }
+}
+ 
